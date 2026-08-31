008d84d39efa1702a2188074feb39d8c81452253
Azlan Latif <azlan.latif@gmail.com>
2026-08-31 21:14:36 +0100

    Added method to find angles and positions

--- linkage.py
+++ linkage.py
@@ -21,6 +21,49 @@ if min(lengths) + max(lengths) > sum(lengths) - min(lengths) - max(lengths):
 else:
     print("Grashof condition satisfied — crank can fully rotate.")
 
+#--------------------------------------------------------------------------
+
+import numpy as np #library for working with arrays
+from scipy.optimize import fsolve #finding the roots of a function
+
+
+def loop_equations(unknowns, theta2): #function defined named loop equations, and takes two inputs, unknowns and theta2. Unknowns are the guesses we make for 03 and 04, and 02 is known
+    """
+    The two vector loop equations that must both equal zero.
+    unknowns = [theta3, theta4] -- what we're solving for.
+    theta2 -- the crank angle we've chosen (input, in radians).
+    """
+    theta3, theta4 = unknowns
+    eq1 = CRANK * np.cos(theta2) + COUPLER * np.cos(theta3) - ROCKER * np.cos(theta4) - GROUND #vector equation of X lengths crank + coupler - rocker - ground = 0
+    eq2 = CRANK * np.sin(theta2) + COUPLER * np.sin(theta3) - ROCKER * np.sin(theta4) #vector equation of Y lengths crank + coupler - rocker = 0
+    return [eq1, eq2] #outputted when we use this function, the two equations must equal zero for the loop to close, otherwise will keep adjusting 03 and 04
+
+
+def solve_position(theta2, initial_guess=(1.0, 2.0)): #numerical methods approach to find 03 and 04, given 02 and an initial guess for 03 and 04
+    """
+    Given a crank angle theta2 (radians), find the coupler angle (theta3)
+    and rocker angle (theta4) that close the loop.
+    """
+    theta3, theta4 = fsolve(loop_equations, initial_guess, args=(theta2,)) #solve for 0
+    return theta3, theta4 #function outputs the values of 03 and 04 that give 0, given 02
+
+#------------------------------------------------------------------------------------------------------------------
+
+def get_joint_positions(theta2, theta3, theta4): #get the x,y coordintes of the four joints in the linkage, given the angles of the three moving links
+    O2 = (0, 0) #O2 is our initial crank so stays at the origin
+    O4 = (GROUND, 0) #O4 depends on the length of our ground linkage but is completely horizontal from O2
+    A = (CRANK * np.cos(theta2), CRANK * np.sin(theta2)) #Using basic trig to find x,y coordinates of A, which is the point where crank and coupler meet
+    B = (O4[0] + ROCKER * np.cos(theta4), O4[1] + ROCKER * np.sin(theta4)) #Using basic trig to find x,y coordinates of B, which is the point where coupler and rocker meet
+    return O2, A, B, O4 #Output the coordinates of the four joints in the linkage, which can be used to draw the linkage
 
 if __name__ == "__main__":
     print(f"Ground: {GROUND}, Crank: {CRANK}, Coupler: {COUPLER}, Rocker: {ROCKER}")
+
+    theta2_rad = np.radians(45) #convert 45 degrees to radians
+    theta3, theta4 = solve_position(theta2_rad) #using solve position function to find 03 and 04 given 02 = 45 degrees
+    print(f"\nAt crank angle 45°:")
+    print(f"Coupler angle: {np.degrees(theta3):.2f}°")
+    print(f"Rocker angle: {np.degrees(theta4):.2f}°") #converts output from radians to degrees and prints it out
+    print(f"Joint positions: {get_joint_positions(theta2_rad, theta3, theta4)}") #output x,y positions of all joints
+
+    #now that we can find the values of 03 and 04 given 02, we need to work on drawing them
